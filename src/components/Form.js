82261ed789e767ec9c337709696a814d10be2a55
jonguzman5 <jonguzman5@gmail.com>
2020-10-06 16:11:58 -0400

    handle form components (handleChange, onFormSubmit)

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,34 +3,38 @@ import React, {Component} from 'react';
 class Form extends Component {
     constructor(props) {
         super(props);
-        
-        /*
-            TODO - set initial state for link name and URL 
-
-        */
+        this.state = {
+            name: "",
+            URL: ""
+        }
     }
 
     handleChange = event => {
-        /*
-            TODO - Logic for changing state based on form changes
-        */
+       this.setState({
+           [event.target.name]: event.target.value
+        });
     }
 
     onFormSubmit = (event) => {
-        // to prevent page reload on form submit
         event.preventDefault();
-        
-        /*
-            TODO - Logic for calling props to handle submission and setting state changes
-        */
-
+        const favLink = {
+            name: this.state.name,
+            URL: this.state.URL
+        }
+        this.props.handleSubmit(favLink)
     }
 
     render() {
 
         return(
-            <form>
-                {/* TODO - Logic for returning a form element with labels and inputs for link name and URL */}
+            <form onSubmit={this.onFormSubmit}>
+                <label>Name: {this.state.name}</label>
+                <input name="name" value={this.state.name} onChange={this.handleChange}/>
+                <label>URL: {this.state.URL}</label>
+                <input name="URL" value={this.state.URL} onChange={this.handleChange}/>
+                <br/>
+                <br/>
+                <input type="submit" value="Submit" />
             </form>
         )
     
--- src/components/LinkContainer.js
+++ src/components/LinkContainer.js
@@ -12,18 +12,15 @@ class LinkContainer extends React.Component {
     }
 
     removeCharacter = index => {
-        /*
-            TODO - Create logic for setting the state to filter array and remove favLink at index
-        */
        this.setState({
            favLinks: this.state.favLinks.filter((link, i) => i !== index)
        })
     }
 
     handleSubmit = favLink => {
-        /*
-            TODO - Create logic to setState and add new favLink to favLinks array in state
-        */
+       this.setState({
+           favLinks: this.state.favLinks.concat(favLink)
+       })
     }
 
     render() {
@@ -40,7 +37,7 @@ class LinkContainer extends React.Component {
 
                 <h3>Add New</h3>
                 <Form
-                
+                    handleSubmit={this.handleSubmit}
                 />
             </div>
         );
